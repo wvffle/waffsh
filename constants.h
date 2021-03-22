d6bc8f25cdd1fab131676911908f686341a74c0e
Kasper Seweryn <github@wvffle.net>
2021-03-22 23:03:28 +0100

    Use getline instead of getchar

--- constants.h
+++ constants.h
@@ -1,7 +1,6 @@
 #pragma once
 
 #define PROMPT "> "
-#define READLINE_BUFFER_SIZE 1024
 
 #define PARSER_TOKEN_DELIMETERS "\t "
 #define PARSER_TOKEN_BUFFER_SIZE 8
--- read.c
+++ read.c
@@ -1,37 +1,22 @@
 #include "read.h"
-#include "constants.h"
 
 #include <stdlib.h>
 #include <syslog.h>
 #include <stdio.h>
 
 char* read_line () {
-    unsigned int size = READLINE_BUFFER_SIZE;
-    char* buf = malloc(sizeof(char) * size);
-    if (buf == NULL) {
-        syslog(LOG_ERR, "read_line allocation error.");
+    char* line = NULL;
+    ssize_t size = 0;
+
+    if (getline(&line, &size, stdin) == -1) {
+        // NOTE: Reached EOF, we can terminate successfully
+        if (feof(stdin)) {
+            exit(EXIT_SUCCESS);
+        }
+
+        syslog(LOG_ERR, "read_line can't read the line.");
         exit(EXIT_FAILURE);
     }
 
-    int i = 0;
-    while (1) {
-        // WARN: Thread unsafe
-        char c = getchar_unlocked();
-
-        if (c == EOF || c == '\n') {
-            buf[i] = '\n';
-            return buf;
-        }
-
-        buf[i] = c;
-        if (++i >= size) {
-            size += READLINE_BUFFER_SIZE;
-            buf = realloc(buf, size);
-
-            if (buf == NULL) {
-                syslog(LOG_ERR, "read_line reallocation error.");
-                exit(EXIT_FAILURE);
-            }
-        }
-    }
+    return line;
 }

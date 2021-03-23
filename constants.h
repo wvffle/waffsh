27557e10abed0145351182ca7094d19ddfc52bb4
Kasper Seweryn <github@wvffle.net>
2021-03-23 01:37:27 +0100

    Fix carriage return

--- constants.h
+++ constants.h
@@ -5,5 +5,5 @@
 // NOTE: The \n is within the delimeters because
 //       execvp was failing due to execution of
 //       arguments like 'firefox\n'
-#define PARSER_TOKEN_DELIMETERS "\n\t\c "
+#define PARSER_TOKEN_DELIMETERS "\n\t\r "
 #define PARSER_TOKEN_BUFFER_SIZE 8

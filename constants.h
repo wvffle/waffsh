e260fdffb546e95de50f5e15cdaed443bbcc665f
Kasper Seweryn <github@wvffle.net>
2021-03-23 09:42:15 +0100

    Add vertical tab and page break to parser delimeters

--- constants.h
+++ constants.h
@@ -7,5 +7,5 @@
 // NOTE: The \n is within the delimeters because
 //       execvp was failing due to execution of
 //       arguments like 'firefox\n'
-#define PARSER_TOKEN_DELIMETERS "\n\t\r "
+#define PARSER_TOKEN_DELIMETERS "\n\t\r\v\f "
 #define PARSER_TOKEN_BUFFER_SIZE 8
